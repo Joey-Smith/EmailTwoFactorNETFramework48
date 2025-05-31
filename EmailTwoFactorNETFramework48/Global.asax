810be559017e630903630962e77159ab8cd76159
Joey Smith <joey.m.smith@outlook.com>
2025-05-31 07:34:10 -0700

    Add project files.

---
+++ EmailTwoFactorNETFramework48/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmailTwoFactorNETFramework48.MvcApplication" Language="C#" %>
---
+++ EmailTwoFactorNETFramework48/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmailTwoFactorNETFramework48.Startup))]
+namespace EmailTwoFactorNETFramework48
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
